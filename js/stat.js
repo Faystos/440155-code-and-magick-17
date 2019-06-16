7cef4bfec368dc5c0df60a6d9d3248db9e764e5a
Faystos <fayst_@bk.ru>
2019-06-17 02:12:34 +0300

    поправил код согласно код гайду

--- js/stat.js
+++ js/stat.js
@@ -1,8 +1,4 @@
-//Размеры окна
-var CLOUD_WIDTH = 500;
-var CLOUD_HEIGHT = 200;
-//
-
+'use strict';
 var CLOUD_WIDTH = 500;
 var CLOUD_HEIGHT = 200;
 var CLOUD_X = 100;
@@ -13,12 +9,12 @@ var TEXT_WIDTH = 50;
 var BAR_HEIGHT = 20;
 var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -30,7 +26,7 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, times) {
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
