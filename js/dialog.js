c8910c289c81f3a596a0f18cb860d825262dc5ee
Faystos <fayst_@bk.ru>
2019-07-23 02:23:55 +0300

    закончил работу над заданием:  'Учебный проект: в движении'

--- js/dialog.js
+++ js/dialog.js
@@ -2,10 +2,20 @@
 
 (function () {
 
+// Перетаскивам окно настроек визардв
+
   var setupDialogElement = document.querySelector('.setup');
   var dialogHandler = setupDialogElement.querySelector('.upload');
 
-  dialogHandler.addEventListener('mousedown', function (evt) {
+
+  var onClickPreventDefault = function (evt) {
+    evt.preventDefault();
+
+    dialogHandler.removeEventListener('click', onClickPreventDefault);
+  };
+
+
+  var onMouseDown = function (evt) {
     evt.preventDefault();
 
     var startCoords = {
@@ -31,7 +41,6 @@
 
       setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
       setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
-
     };
 
     var onMouseUp = function (upEvt) {
@@ -41,84 +50,45 @@
       document.removeEventListener('mouseup', onMouseUp);
 
       if (dragged) {
-        var onClickPreventDefault = function (evt) {
-          evt.preventDefault();
-          dialogHandler.removeEventListener('click', onClickPreventDefault)
-        };
+        onClickPreventDefault(evt);
         dialogHandler.addEventListener('click', onClickPreventDefault);
       }
-
     };
 
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
+  };
+
+  dialogHandler.addEventListener('mousedown', onMouseDown);
+
+  // *************************************************************************
+
+  // Перетаскиваем итем
+
+  var setupShop = document.querySelector('.setup-artifacts-shop');
+  var setupPlayer = document.querySelector('.setup-player');
+  var shopArtifactsCell = setupShop.querySelector('.setup-artifacts-cell');
+  var shopArtifactsCells = setupShop.querySelectorAll('.setup-artifacts-cell');
+  var playerArtifactsCells = setupPlayer.querySelectorAll('.setup-artifacts-cell');
+  var item = shopArtifactsCell.querySelector('img');
+
+  playerArtifactsCells.forEach(function (elem) {
+    elem.addEventListener('dragover', function () {
+      event.preventDefault();
+    });
+    elem.addEventListener('drop', function () {
+      elem.appendChild(item);
+    });
   });
 
+  shopArtifactsCells.forEach(function (elem) {
+    elem.addEventListener('dragover', function () {
+      event.preventDefault();
+    });
+    elem.addEventListener('drop', function () {
+      elem.appendChild(item);
+    });
+  });
+  // ************************************************************************
 
 })();
-
-
-/*
-var onMouseDown = function (evt) {
-  evt.preventDefault();
-
-  var startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
-  var dragged = false;
-
-  var onMouseMove = function (moveEvt) {
-    moveEvt.preventDefault();
-
-    var shift = {
-      x: startCoords.x - moveEvt.clientX,
-      y: startCoords.y - moveEvt.clientY
-    };
-
-    startCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
-    };
-
-    var newBlockX = buttonMain.offsetLeft - shift.x;
-    var newBlockY = buttonMain.offsetTop - shift.y;
-
-    if (newBlockX < mapOverlay.offsetLeft - buttonMain.offsetWidth / 2 ||
-    newBlockY < mapOverlay.offsetTop ||
-    newBlockX + buttonMain.offsetWidth > mapOverlay.offsetLeft + mapOverlay.offsetWidth + (buttonMain.offsetWidth / 2) ||
-    newBlockY + buttonMain.offsetHeight > mapOverlay.offsetTop + mapOverlay.offsetHeight + (buttonMain.offsetHeight / 2)) {
-
-      document.removeEventListener('mousemove', onMouseMove);
-
-    } else {
-      buttonMain.style.top = newBlockY + 'px';
-      buttonMain.style.left = newBlockX + 'px';
-    }
-
-  };
-
-  var onMouseUp = function (upEvt) {
-    upEvt.preventDefault();
-    getCoords(buttonMain);
-
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-
-    if (dragged) {
-      var onClickPreventDefault = function () {
-        buttonMain.preventDefault();
-        buttonMain.removeEventListener('click', onClickPreventDefault);
-      };
-      buttonMain.addEventListener('click', onClickPreventDefault);
-    }
-
-  };
-
-
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-};
-
-buttonMain.addEventListener('mousedown', onMouseDown);
-*/
