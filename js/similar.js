9e3ade6c7a45ec2d27fa79118013e15ac05e01b5
Vladimir <fayst_@bk.ru>
2019-09-21 13:59:06 +0300

    фильтр снова работает, осталось поправить недочеты

--- js/similar.js
+++ js/similar.js
@@ -8,24 +8,58 @@
   var fireballColor;
   var wizards = [];
 
+  var getRank = function (wizard) {
+    var rank = 0;
+
+    if (wizard.colorCoat === coatColor) {
+      rank += 2;
+    }
+    if (wizard.colorEyes === eyesColor) {
+      rank += 1;
+    }
+    if (wizard.colorFireball === fireballColor) {
+      rank += 1;
+    }
+
+    return rank;
+  };
+
+  var namesComparator = function (left, right) {
+    if (left > right) {
+      return 1;
+    } else if (left < right) {
+      return -1;
+    } else {
+      return 0;
+    }
+  };
+
   var updateWizards = function () {
 
-    var sameCoatWizards = wizards.filter(function (it) {
-      return it.colorCoat === coatColor;
-    });
-    var sameEyesWizards = wizards.filter(function (it) {
-      return it.colorEyes === eyesColor;
-    });
-
-    var filteredWizards = sameCoatWizards.concat(sameEyesWizards).concat(wizards);
-
-    var uniqueWizards =
-    filteredWizards.filter(function (it, i) {
-      return filteredWizards.indexOf(it) === i;
-    });
-
-    window.render(uniqueWizards);
+    window.render(wizards.sort(function (left, right) {
+      var rankDiff = getRank(right) - getRank(left);
+      if (rankDiff === 0) {
+        rankDiff = namesComparator(left.name, right.name);
+      }
+      return rankDiff;
+    }));
   };
+
+  window.wizard.onEyesChange = function (color) {
+    eyesColor = color;
+    updateWizards();
+  };
+
+  window.wizard.onCoatChange = function (color) {
+    coatColor = color;
+    updateWizards();
+  };
+
+  window.wizard.onFirebalChange = function (color) {
+    fireballColor = color;
+    updateWizards();
+  };
+
 
   // // Изменяем цвет fireball «огненный шар»
   // var handlerClickSetupFireball = function (evt) {
--- js/wizard.js
+++ js/wizard.js
@@ -3,62 +3,62 @@
 (function () {
 
   var wizard = {
-      onEyesChange: function (color) {},
-      onCoatChange: function (color) {},
-      onFirebalChange: function (color) {}
-    };
+    onEyesChange: function (color) {},
+    onCoatChange: function (color) {},
+    onFirebalChange: function (color) {}
+  };
 
     // Изменяем цвет fireball «огненный шар»
-    var handlerClickSetupFireball = function (evt) {
-      evt.preventDefault();
-      var newColor = window.data.COLORSFIREBALLS[window.data.countPress];
-      window.data.setupFireballWrap.style.background = newColor;
-      window.data.countPress++;
+  var handlerClickSetupFireball = function (evt) {
+    evt.preventDefault();
+    var newColor = window.data.COLORSFIREBALLS[window.data.countPress];
+    window.data.setupFireballWrap.style.background = newColor;
+    window.data.countPress++;
 
-      if (window.data.countPress === window.data.COLORSFIREBALLS.length) {
-        window.data.countPress = 0;
-      }
-      wizard.onFirebalChange(newColor);
+    if (window.data.countPress === window.data.COLORSFIREBALLS.length) {
+      window.data.countPress = 0;
+    }
+    wizard.onFirebalChange(newColor);
 
-      // fireballColor = newColor;
-      // updateWizards();
-    };
+    // fireballColor = newColor;
+    // updateWizards();
+  };
 
-    window.data.setupFireballWrap.addEventListener('click', handlerClickSetupFireball);
-    // *************************************************************************
-    // Меняем цвет глаз Визарда
+  window.data.setupFireballWrap.addEventListener('click', handlerClickSetupFireball);
+  // *************************************************************************
+  // Меняем цвет глаз Визарда
 
-    var handlerClickSetupWizardEyes = function (evt) {
-      evt.preventDefault();
-      var newColor = window.data.WIZARD_EYES_COLOR[window.data.countPress];
-      window.data.wizardEyes.style.fill = newColor;
-      window.data.countPress++;
+  var handlerClickSetupWizardEyes = function (evt) {
+    evt.preventDefault();
+    var newColor = window.data.WIZARD_EYES_COLOR[window.data.countPress];
+    window.data.wizardEyes.style.fill = newColor;
+    window.data.countPress++;
 
-      if (window.data.countPress === window.data.WIZARD_EYES_COLOR.length) {
-        window.data.countPress = 0;
-      }
-      wizard.onEyesChange(newColor);
+    if (window.data.countPress === window.data.WIZARD_EYES_COLOR.length) {
+      window.data.countPress = 0;
+    }
+    wizard.onEyesChange(newColor);
 
-      // eyesColor = newColor;
-      // updateWizards();
-    };
+    // eyesColor = newColor;
+    // updateWizards();
+  };
 
-    window.data.setupWizard.addEventListener('click', handlerClickSetupWizardEyes);
-    // ************************************************************************
+  window.data.setupWizard.addEventListener('click', handlerClickSetupWizardEyes);
+  // ************************************************************************
 
-    // Меняем цвет плаща произвольным свойством
+  // Меняем цвет плаща произвольным свойством
 
-    var handlerClickSetupWizardCoat = function (evt) {
-      evt.preventDefault();
-      var newColor = window.data.WIZARD_COAT_COLOR[window.getRandomInt(0, window.data.WIZARD_COAT_COLOR.length - 1)];
-      window.data.wizardCoat.style.fill = newColor;
-      wizard.onCoatChange(newColor);
+  var handlerClickSetupWizardCoat = function (evt) {
+    evt.preventDefault();
+    var newColor = window.data.WIZARD_COAT_COLOR[window.getRandomInt(0, window.data.WIZARD_COAT_COLOR.length - 1)];
+    window.data.wizardCoat.style.fill = newColor;
+    wizard.onCoatChange(newColor);
 
-      // coatColor = newColor;
-      // updateWizards();
-    };
+    // coatColor = newColor;
+    // updateWizards();
+  };
 
-    window.data.setupWizard.addEventListener('click', handlerClickSetupWizardCoat);
+  window.data.setupWizard.addEventListener('click', handlerClickSetupWizardCoat);
 
-    return window.wizard = wizard;
+  return window.wizard = wizard;
 })();
