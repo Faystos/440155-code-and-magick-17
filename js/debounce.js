b4114a528f7aad65c475a49c130ef22093a2917a
Vladimir <fayst_@bk.ru>
2019-10-08 15:57:01 +0300

    закончил работу над фильтрацией похожих магов и выполнил функцию устранения 'дребизга'.

--- js/debounce.js
+++ js/debounce.js
@@ -1,6 +1,6 @@
 'use strict';
 (function () {
-  var DEBOUNCE_INTERVAL = 30; // ms
+  var DEBOUNCE_INTERVAL = 300; // ms
   
   window.debounce = function (cb) {
     var lastTimeout = null;
--- js/similar.js
+++ js/similar.js
@@ -20,7 +20,7 @@
     return rank;
   };  
 
-  var updateWizards = function () {
+  window.updateWizards = function () {
 
     window.render(wizards.slice().
       sort(function (left, right) {
@@ -32,74 +32,20 @@
       }));
   };
 
-  window.wizard.onEyesChange = function (color) {
+  window.wizard.onEyesChange = window.debounce(function (color) {
     eyesColor = color;
-    updateWizards();
-  };
+    updateWizards();    
+  });
 
-  window.wizard.onCoatChange = function (color) {
+  window.wizard.onCoatChange = window.debounce(function (color) {
     coatColor = color;
-    updateWizards();
-  };
+    updateWizards();    
+  });
 
   window.wizard.onFirebalChange = function (color) {
     fireballColor = color;
-    updateWizards();
-  };
-
-
-  // // Изменяем цвет fireball «огненный шар»
-  // var handlerClickSetupFireball = function (evt) {
-  //   evt.preventDefault();
-  //   var newColor = window.data.COLORSFIREBALLS[window.data.countPress];
-  //   window.data.setupFireballWrap.style.background = newColor;
-  //   window.data.countPress++;
-  //
-  //   if (window.data.countPress === window.data.COLORSFIREBALLS.length) {
-  //     window.data.countPress = 0;
-  //   }
-  //
-  //   fireballColor = newColor;
-  //   updateWizards();
-  // };
-  //
-  // window.data.setupFireballWrap.addEventListener('click', handlerClickSetupFireball);
-  // // *************************************************************************
-  // // Меняем цвет глаз Визарда
-  //
-  // var handlerClickSetupWizardEyes = function (evt) {
-  //   evt.preventDefault();
-  //   var newColor = window.data.WIZARD_EYES_COLOR[window.data.countPress];
-  //   window.data.wizardEyes.style.fill = newColor;
-  //   window.data.countPress++;
-  //
-  //   if (window.data.countPress === window.data.WIZARD_EYES_COLOR.length) {
-  //     window.data.countPress = 0;
-  //   }
-  //
-  //   eyesColor = newColor;
-  //   updateWizards();
-  // };
-  //
-  // window.data.setupWizard.addEventListener('click', handlerClickSetupWizardEyes);
-  // // ************************************************************************
-  //
-  // // Меняем цвет плаща произвольным свойством
-  //
-  // var handlerClickSetupWizardCoat = function (evt) {
-  //   evt.preventDefault();
-  //   var newColor = window.data.WIZARD_COAT_COLOR[window.getRandomInt(0, window.data.WIZARD_COAT_COLOR.length - 1)];
-  //   window.data.wizardCoat.style.fill = newColor;
-  //   coatColor = newColor;
-  //   updateWizards();
-  // };
-  //
-  // window.data.setupWizard.addEventListener('click', handlerClickSetupWizardCoat);
-  // ************************************************************************
-
-  // ***************************************************************************
-
-  // **************************Загрузка данных********************************
+    updateWizards();    
+  };  
 
   var onLoad = function (data) {
     wizards = data;
